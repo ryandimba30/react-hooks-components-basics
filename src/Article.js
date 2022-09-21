5d78043bea5afd730ee9d441ae3eda53f1fdd457
ryandimba <ryan.dimba@student.moringaschool.com>
2022-09-21 20:28:09 +0300

    Completed assignment

--- src/Article.js
+++ src/Article.js
@@ -5,5 +5,8 @@ function Article() {
     <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
   );
 }
+function Comment() {
+  return <div>Naturally, I agree with this article.</div>;
+}
 
 export default Article;
